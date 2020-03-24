8b2f2d89283af26ab55cb077f96b3d33d931ea7f
Konstantin Bukin <bukin@inetstudio.ru>
2020-03-24 19:23:50 +0300

    Update to 7.0

--- entities/favorites/src/Contracts/Http/Requests/Front/AddRequestContract.php
+++ entities/favorites/src/Contracts/Http/Requests/Front/AddRequestContract.php
@@ -1,12 +1,9 @@
 <?php
 
 namespace InetStudio\FavoritesPackage\Favorites\Contracts\Http\Requests\Front;
-
-use InetStudio\AdminPanel\Base\Contracts\Http\Requests\BaseRequestContract;
-
 /**
  * Interface AddRequestContract.
  */
-interface AddRequestContract extends BaseRequestContract
+interface AddRequestContract
 {
 }
--- entities/favorites/src/Contracts/Http/Requests/Front/RemoveRequestContract.php
+++ entities/favorites/src/Contracts/Http/Requests/Front/RemoveRequestContract.php
@@ -1,12 +1,9 @@
 <?php
 
 namespace InetStudio\FavoritesPackage\Favorites\Contracts\Http\Requests\Front;
-
-use InetStudio\AdminPanel\Base\Contracts\Http\Requests\BaseRequestContract;
-
 /**
  * Interface RemoveRequestContract.
  */
-interface RemoveRequestContract extends BaseRequestContract
+interface RemoveRequestContract
 {
 }
